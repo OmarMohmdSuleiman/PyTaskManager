ab23a03013c8b33369d5e916a7f8a4370baf84d5
Omar <omarsuleiman739@gmail.com>
2024-03-27 22:35:16 +0200

    Created the constructor for the Task class.

--- Assignment-4-OmarSuleiman.py
+++ Assignment-4-OmarSuleiman.py
@@ -3,6 +3,14 @@ class Node:
     def __init__(self,data):
         self.__data=data
         self.__next=None
+#Task class
+class Task:
+    def __init__(self,description:str,priority:int,completed:bool):
+        self.__task_id=0
+        self.__description=description
+        self.__priority=priority
+        self.__completed=completed
+
 
 
         
