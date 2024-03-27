4ed18e2d5441a1df71ae75c125bb9dfc0a2273ca
Omar <omarsuleiman739@gmail.com>
2024-03-27 22:55:34 +0200

    Created Getters and Setters for Task class.

--- Assignment-4-OmarSuleiman.py
+++ Assignment-4-OmarSuleiman.py
@@ -5,11 +5,28 @@ class Node:
         self.__next=None
 #Task class
 class Task:
-    def __init__(self,description:str,priority:int,completed:bool):
+    def __init__(self,description:str,priority:int,completed=False):
         self.__task_id=0
         self.__description=description
         self.__priority=priority
         self.__completed=completed
+    #Getters and Setters
+    def getTaskId(self):
+        return self.__task_id
+    def getDescription(self):
+        return self.__description
+    def getPriority(self):
+        return self.__priority
+    def setPriority(self,priority):
+        self.__priority=priority
+    def getCompleted(self):
+        return self.__completed
+    def setCompleted(self,completed):
+        self.__completed=True
+        
+
+
+        
 
 
 
