c1ebd056e508cf17f1d7d7b1331032b3889fd27c
Omar <omarsuleiman739@gmail.com>
2024-03-27 23:53:14 +0200

    Created priority queue method.

--- Assignment-4-OmarSuleiman.py
+++ Assignment-4-OmarSuleiman.py
@@ -3,12 +3,39 @@ class Node:
     def __init__(self,data):
         self.__data=data
         self.__next=None
+    def getData(self):
+        return self.__data
+    def getNext(self):
+        return self.__next
+    def setNext(self,new_next):
+        self.__next=new_next
 #Linked List class
 class LinkedList:
     def __init__(self):
         self.__header=None
     def isEmpty(self):
         return self.__header==None
+    #add priority queue
+    def addNodeWithPriority(self,new_node : Node) -> None:
+
+        if (self.isEmpty()):
+            self.__header = new_node
+            return
+
+        if (new_node.getData() > self.__header.getData()):
+            new_node.setNext(self.__header)
+            self.__header = new_node
+
+        current = self.__header 
+        previous =  self.__header 
+
+        while (current is not None and current.getData() >= new_node.getData() ):
+            previous = current
+            current = current.getNext()
+
+
+        previous.setNext(new_node)
+        new_node.setNext(current)
     
 #Task class
 class Task:
